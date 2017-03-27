4da65eb9d78b87c6392085504401f24f16216e0a
hamzazakaria <hamza.zakaria9@gmail.com>
2017-03-27 23:42:08 +0100

    pandawa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    BASMA
+                    PANDAWA
                 </div>
 
                 <div class="links">
