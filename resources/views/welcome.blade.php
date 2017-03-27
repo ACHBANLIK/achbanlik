4a18c9398ad3f39744bdaf91ce0ed3250c418317
hamzazakaria <hamza.zakaria9@gmail.com>
2017-03-27 23:26:01 +0100

    basma

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BASMA
                 </div>
 
                 <div class="links">
